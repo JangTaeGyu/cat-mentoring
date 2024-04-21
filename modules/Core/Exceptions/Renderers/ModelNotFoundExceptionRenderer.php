e429d6cfea2e608925d16bb9ee0b5c5a768e87e5
JangTaeGyu <ttggbbgg2@gmail.com>
2024-04-21 22:06:12 +0900

    http status code 추가

--- modules/Core/Exceptions/Renderers/ModelNotFoundExceptionRenderer.php
+++ modules/Core/Exceptions/Renderers/ModelNotFoundExceptionRenderer.php
@@ -2,20 +2,19 @@
 
 namespace Modules\Core\Exceptions\Renderers;
 
-use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ModelNotFoundExceptionRenderer implements Renderable
 {
     public function render($request, \Exception $e): mixed
     {
-        return response()->json(
-            [
-                'code' => ResponseAlias::HTTP_NOT_FOUND,
-                'error' => [
-                    'message' => 'model not found',
-                ],
+        $httpStatusCode = ResponseAlias::HTTP_NOT_FOUND;
+
+        return response()->json([
+            'code' => $httpStatusCode,
+            'error' => [
+                'message' => 'model not found',
             ],
-        );
+        ], $httpStatusCode);
     }
 }
